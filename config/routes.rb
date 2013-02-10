7eb13e310e089c7f7017f01e90ebde53e87e5925
larshidde <ziplarson@gmail.com>
2013-02-10 13:07:16 -0800

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 SampleApp::Application.routes.draw do
   
 
+  get "users/new"
+
   # get "static_pages/home"
   # root_path => '/'
   # root_url  => 'http://localhost:3000/'
   
   root to: 'static_pages#home'
 
-
+  match '/signup',  to: 'users#new'
+  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
